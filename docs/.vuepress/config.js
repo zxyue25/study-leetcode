24bb1bf6b217b74dc1e0d0642e3373c934ff23ea
zxyue25 <zxy@syzc.net.cn>
2021-06-21 00:52:58 +0800

    feat:vuepress搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "leetcode-javascript",
+  description: "Using JavaScript to brush questions in leetcode",
+  themeConfig: {
+    nav: [],
+    sidebar: [
+      {
+        title: "指南", // 必要的
+        path: "/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: [],
+      },
+      {
+        title: "simple", // 必要的
+        path: "/simple/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ["/simple/01.html", "/simple/20.html", "/simple/58.html"],
+      },
+    ],
+  },
+};
