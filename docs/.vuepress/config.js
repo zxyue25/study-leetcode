92be3669ec912e200468a1f4e45cd1740a20be91
zxyue25 <zxy@syzc.net.cn>
2021-07-17 18:04:16 +0800

    add: 121买卖股票的最佳时机

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
         title: "simple", // 必要的
         path: "/simple/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         sidebarDepth: 1, // 可选的, 默认值是 1
-        children: ["/simple/01.html","/simple/14.html","/simple/20.html", "/simple/35.html","/simple/58.html", "/simple/66.html"],
+        children: ["/simple/01.html","/simple/14.html","/simple/20.html", "/simple/35.html","/simple/58.html", "/simple/66.html","/simple/121.html"],
       },
     ],
   },
